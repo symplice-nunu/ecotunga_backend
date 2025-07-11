ca8a8a751609282f060ea7d80f6b544f56812b65
symplice <symplice@techaffinity.com>
2025-07-11 21:39:47 +0530

    Update knexfile.js configuration

--- knexfile.js
+++ knexfile.js
@@ -13,5 +13,18 @@ module.exports = {
       tableName: 'knex_migrations',
       directory: './migrations'
     }
+  },
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    }
   }
 };
